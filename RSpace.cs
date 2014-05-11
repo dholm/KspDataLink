d420d08f84f7b2989559d73474aa158b00b6537e
David Holm <dholmster@gmail.com>
2014-05-11 21:50:11 +0200

    Create toolbar button

--- RSpace.cs
+++ RSpace.cs
@@ -1,42 +1,64 @@
 using System;
+using System.IO;
+using System.Reflection;
 
 using UnityEngine;
-using KSP;
 
 namespace RSpace
 {
     [KSPAddon(KSPAddon.Startup.Flight, false)]
     public class RSpaceFlight : RSpace
     {
-        public override string MonoName
-        {
-            get
-                {
-                    return this.name;
-                }
-        }
     }
 
     public partial class RSpace : MonoBehaviour
     {
-        public virtual String MonoName
+        private IButton toolbarButton = null;
+
+        public static String Name
         {
-            get;
-            set;
+            get
+            {
+                return MethodBase.GetCurrentMethod().DeclaringType.Name;
+            }
         }
 
         public RSpace()
         {
         }
 
+        public void ToolbarInstall()
+        {
+            if (ToolbarManager.ToolbarAvailable)
+            {
+                Logger.debug("Installing toolbar..");
+
+                toolbarButton = ToolbarManager.Instance.add(Name, Name);
+                string buttonOff = String.Format("{0}/Textures/{0}_off", Name);
+                toolbarButton.TexturePath = buttonOff;
+                toolbarButton.ToolTip = String.Format("Open {0}", Name);
+            }
+        }
+
 #region Events
+        public void Start()
+        {
+            Logger.debug("Starting up..");
+
+            DontDestroyOnLoad(this);
+            ToolbarInstall();
+        }
+
         public void Awake()
         {
+            Logger.debug("Waking up..");
+
             DontDestroyOnLoad(this);
         }
 
         public void OnDestroy()
         {
+            Logger.debug("Shutting down..");
         }
 
         public void Update()
